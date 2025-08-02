49d8eb2cf2f553b0b993cdbec887da4aafe48f6b
AliyanAmjad986 <aliyanamjad42@gmail.com>
2025-08-02 15:07:07 +0500

    update project

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ let navbuttons={
     Resume: document.getElementById('Resume'),
     Contact: document.getElementById('Contact')
 } //mainlearning
+let conbuttons ={
+    Github: document.getElementById('Github')
+    // LinkedIn: document.getElementById('LinkedIn'),
+    // Twitter: document.getElementById('Twitter'),
+    // Facebook: document.getElementById('Facebook'),
+    // Instagram: document.getElementById('Instagram')
+}
 
 navbuttons.Home.addEventListener('click', function() {
     window.location.href = 'index.html'; //use when i click on home button the home html open
@@ -15,3 +22,9 @@ navbuttons.Home.addEventListener('click', function() {
 navbuttons.Skills.addEventListener('click', function() {
     window.location.href = 'Skills.html';
 }); 
+navbuttons.Education.addEventListener('click', function() {
+    window.location.href = 'education.html';
+});
+Github.addEventListener('click', function() {
+    window.open('https://github.com/AliyanAmjad986', '_blank'); // Open GitHub in a new tab;
+});
