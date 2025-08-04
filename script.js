1e5a1ebe59e669a4e4cda6d8059e0232df2bf367
AliyanAmjad986 <aliyanamjad42@gmail.com>
2025-08-04 15:04:11 +0500

    update project

--- script.js
+++ script.js
@@ -12,18 +12,27 @@ let conbuttons ={
     Github: document.getElementById('Github'),
     Linkedln: document.getElementById('LinkedIn'),
     Email: document.getElementById('Email')
-    
 }
 
 navbuttons.Home.addEventListener('click', function() {
     window.location.href = 'index.html'; //use when i click on home button the home html open
 });
 navbuttons.Skills.addEventListener('click', function() {
-    window.location.href = 'Skills.html';
+    window.location.href = 'skills.html';
 }); 
 navbuttons.Education.addEventListener('click', function() {
     window.location.href = 'education.html';
 });
+navbuttons.Projects.addEventListener('click', function() {
+    window.location.href = 'project.html'; //use when i click on projects button the projects html open
+});
+navbuttons.Resume.addEventListener('click', function() {
+    window.location.href = 'resume.html'; //use when i click on resume button the resume html open
+});
+navbuttons.Contact.addEventListener('click', function() {
+    window.location.href = 'contact.html'; //use when i click on contact button the contact html open
+}); 
+
 Github.addEventListener('click', function() {
     window.open('https://github.com/AliyanAmjad986', '_blank'); // Open GitHub in a new tab;
 });
@@ -33,3 +42,6 @@ conbuttons.Linkedln.addEventListener('click', function() {
 conbuttons.Email.addEventListener('click', function () {
     window.open('https://mail.google.com/mail/?view=cm&fs=1&to=aliyanamjad54@gmail.com&su=Subject%20Here&body=Your%20message%20here','_blank');
 });
+  
+
+ 
