8a1a01a15691010b091a1d06f361773c1f9417b7
AliyanAmjad986 <aliyanamjad42@gmail.com>
2025-08-05 10:32:42 +0500

    Update Project with using statement conditionss

--- script.js
+++ script.js
@@ -7,15 +7,23 @@ let navbuttons={
     Projects: document.getElementById('Projects'),
     Resume: document.getElementById('Resume'),
     Contact: document.getElementById('Contact')
-} //mainlearning
+}
+ //mainlearning
 let conbuttons ={
     Github: document.getElementById('Github'),
     Linkedln: document.getElementById('LinkedIn'),
     Email: document.getElementById('Email')
 }
+let projectcard ={
+    project1: document.getElementById('project1'),
+    project2: document.getElementById('project2')
+    // project3: document.getElementById('project3'),
+    // project4: document.getElementById('project4')
+}
 
 navbuttons.Home.addEventListener('click', function() {
     window.location.href = 'index.html'; //use when i click on home button the home html open
+    
 });
 navbuttons.Skills.addEventListener('click', function() {
     window.location.href = 'skills.html';
@@ -42,6 +50,33 @@ conbuttons.Linkedln.addEventListener('click', function() {
 conbuttons.Email.addEventListener('click', function () {
     window.open('https://mail.google.com/mail/?view=cm&fs=1&to=aliyanamjad54@gmail.com&su=Subject%20Here&body=Your%20message%20here','_blank');
 });
-  
 
- 
+
+if(projectcard.project1){
+    projectcard.project1.addEventListener('click', function() {
+        window.open('https://github.com/AliyanAmjad986/HTML-CSS', '_blank'); // Open project1 in a new tab
+    });
+}
+if(projectcard.project2){
+    projectcard.project2.addEventListener('click', function() {
+        window.open('https://github.com/AliyanAmjad986/Internship-projects', '_blank'); // Open project1 in a new tab
+    });
+}
+
+
+      function morecontent(button) {
+      const container = button.closest('.project-info');
+      const description = container.querySelector('.project-discription');
+
+      if (description.classList.contains('morecontent')) {
+        description.classList.remove('morecontent');
+        button.textContent = 'Read less';
+      } else {
+        description.classList.add('morecontent');
+        button.textContent = 'Read more';
+      }
+    }
+
+   
+
+
